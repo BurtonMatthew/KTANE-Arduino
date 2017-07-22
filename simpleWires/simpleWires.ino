d4ec2e191e2915bfe3f8a457f6604f1a4ea2cbf0
Matt Burton <burtonmatthewa@gmail.com>
2017-07-22 00:35:46 -0400

    Moved last two pins onto non-IDC pins, added logic for having some pins driven by external pullups instead of internal, use an RG led for completion.

--- simpleWires/simpleWires.ino
+++ simpleWires/simpleWires.ino
@@ -1,22 +1,36 @@
 // Keep Talking and Noboby Explodes Simple Wires Module for Arduino
 // By Matthew Burton
 
+#include <BicolourLED.h>
+
 #define NUM_WIRES 6
 #define WIRE0 A0
 #define WIRE1 A1
 #define WIRE2 A2
-#define WIRE3 A3
-#define WIRE4 A4
-#define WIRE5 A5
+#define WIRE3 A3 // Pins A4 and A5 needed for I2C
+#define WIRE4 A6
+#define WIRE5 A7
 
+#define LED_RED 11
+#define LED_GREEN 12
+
+// Read values for pins using the arduino's internal pullup resistors
 #define BLACK_READ 987  // 1M ohm
 #define BLUE_READ 971   // 680k
 #define RED_READ 950    // 470k
 #define YELLOW_READ 913 // 300k
 #define WHITE_READ 879  // 220k
-#define READ_VARIANCE 5
 
-enum class WireColour
+// Read values for pins using an external pullup resistor, which may have a different resistance than the internal pullup
+#define BLACK_READ_EXT 973  // 1M ohm
+#define BLUE_READ_EXT 952   // 680k
+#define RED_READ_EXT 923    // 470k
+#define YELLOW_READ_EXT 875 // 300k
+#define WHITE_READ_EXT 831  // 220k
+
+#define READ_VARIANCE 8
+
+enum class WireColour : int8_t
 {
   None = -1,
   Black = 0,
@@ -27,38 +41,41 @@ enum class WireColour
   MAX
 };
 
-constexpr int16_t colourVoltages[static_cast<size_t>(WireColour::MAX)] = { BLACK_READ, BLUE_READ, RED_READ, YELLOW_READ, WHITE_READ };                                  
+constexpr int16_t readValues_internal[static_cast<size_t>(WireColour::MAX)] = { BLACK_READ, BLUE_READ, RED_READ, YELLOW_READ, WHITE_READ };
+constexpr int16_t readValues_external[static_cast<size_t>(WireColour::MAX)] = { BLACK_READ_EXT, BLUE_READ_EXT, RED_READ_EXT, YELLOW_READ_EXT, WHITE_READ_EXT };
 constexpr int8_t wirePins[NUM_WIRES] = { WIRE0, WIRE1, WIRE2, WIRE3, WIRE4, WIRE5 };
+constexpr const int16_t* readValues[NUM_WIRES] = { readValues_internal, readValues_internal, readValues_internal, readValues_internal, readValues_external, readValues_external };
 
+BicolourLED led(LED_RED, LED_GREEN);
 WireColour lastWires[6];
 int8_t solution;
 
-void setup() 
+void setup()
 {
-    for(int8_t i=0; i<NUM_WIRES; ++i)
-      pinMode(wirePins[i], INPUT_PULLUP);
+  for (int8_t i = 0; i < NUM_WIRES; ++i)
+    pinMode(wirePins[i], INPUT_PULLUP);
 
-    pinMode(13, OUTPUT);
-    Serial.begin(9600);
-    readWires(lastWires);
-    solution = findSolution(lastWires, false);
-    Serial.print(solution);
+  led.begin();
+  readWires(lastWires);
+  solution = findSolution(lastWires, false);
 }
 
-void loop() 
+void loop()
 {
   WireColour currentWires[6];
   readWires(currentWires);
   int8_t cutIndex = getCutIndex(lastWires, currentWires);
-  if(cutIndex != -1)
+  if (cutIndex != -1)
   {
-    if(cutIndex == solution)
-      digitalWrite(13, HIGH);
+    if (cutIndex == solution)
+    {
+      led.write(BicolourLED::Colour::Green);
+    }
     else
     {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
+      led.write(BicolourLED::Colour::Red);
+      delay(225);
+      led.write(BicolourLED::Colour::Off);
     }
     memcpy(lastWires, currentWires, sizeof(lastWires[0]) * NUM_WIRES);
   }
@@ -66,14 +83,14 @@ void loop()
 
 void readWires(WireColour* outWires)
 {
-  for(int8_t i=0; i<NUM_WIRES; ++i)
+  for (int8_t i = 0; i < NUM_WIRES; ++i)
   {
     int16_t wire = analogRead(wirePins[i]);
     outWires[i] = WireColour::None;
-    
-    for(int j=0; j<static_cast<int>(WireColour::MAX); ++j)
+
+    for (int j = 0; j < static_cast<int>(WireColour::MAX); ++j)
     {
-      if(abs(wire - colourVoltages[j]) < READ_VARIANCE)
+      if (abs(wire - readValues[i][j]) < READ_VARIANCE)
       {
         outWires[i] = static_cast<WireColour>(j);
         break;
@@ -84,9 +101,9 @@ void readWires(WireColour* outWires)
 
 int8_t getCutIndex(WireColour* oldWires, WireColour* newWires)
 {
-  for(int8_t i=0; i<NUM_WIRES; ++i)
+  for (int8_t i = 0; i < NUM_WIRES; ++i)
   {
-    if(oldWires[i] != newWires[i])
+    if (oldWires[i] != newWires[i])
     {
       return i;
     }
@@ -101,9 +118,9 @@ int8_t findSolution(WireColour* wires, bool serialOdd)
   int8_t mapping[NUM_WIRES];
   int8_t numWires;
   condenseWires(condensed, mapping, numWires, wires);
-  
+
   int8_t condensedSolution = -1;
-  switch(numWires)
+  switch (numWires)
   {
     case 3:
       condensedSolution = findSolutionThreeWires(condensed, serialOdd);
@@ -119,7 +136,7 @@ int8_t findSolution(WireColour* wires, bool serialOdd)
       break;
   }
 
-  if(condensedSolution != -1)
+  if (condensedSolution != -1)
   {
     return mapping[condensedSolution];
   }
@@ -132,9 +149,9 @@ int8_t findSolution(WireColour* wires, bool serialOdd)
 void condenseWires(WireColour* outCondensed, int8_t* outMapping, int8_t& outNumWires, WireColour* wires)
 {
   outNumWires = 0;
-  for(int8_t i=0; i<NUM_WIRES; ++i)
+  for (int8_t i = 0; i < NUM_WIRES; ++i)
   {
-    if(wires[i] != WireColour::None)
+    if (wires[i] != WireColour::None)
     {
       outCondensed[outNumWires] = wires[i];
       outMapping[outNumWires] = i;
@@ -142,7 +159,7 @@ void condenseWires(WireColour* outCondensed, int8_t* outMapping, int8_t& outNumW
     }
   }
 
-  for(int8_t i=outNumWires; i<NUM_WIRES; ++i)
+  for (int8_t i = outNumWires; i < NUM_WIRES; ++i)
   {
     outCondensed[i] = WireColour::None;
     outMapping[i] = -1;
@@ -151,11 +168,11 @@ void condenseWires(WireColour* outCondensed, int8_t* outMapping, int8_t& outNumW
 
 int8_t findSolutionThreeWires(WireColour* wires, bool serialOdd)
 {
-  if(countWiresByColour(wires, 3, WireColour::Red) == 0)
+  if (countWiresByColour(wires, 3, WireColour::Red) == 0)
     return 1;
-  else if(wires[2] == WireColour::White)
+  else if (wires[2] == WireColour::White)
     return 2;
-  else if(countWiresByColour(wires, 3, WireColour::Blue) > 1)
+  else if (countWiresByColour(wires, 3, WireColour::Blue) > 1)
     return positionOfLastWireByColour(wires, 3, WireColour::Blue);
   else
     return 2;
@@ -163,13 +180,13 @@ int8_t findSolutionThreeWires(WireColour* wires, bool serialOdd)
 
 int8_t findSolutionFourWires(WireColour* wires, bool serialOdd)
 {
-  if(serialOdd && countWiresByColour(wires, 4, WireColour::Red) > 1)
+  if (serialOdd && countWiresByColour(wires, 4, WireColour::Red) > 1)
     return positionOfLastWireByColour(wires, 4, WireColour::Red);
-  else if(wires[3] == WireColour::Yellow && countWiresByColour(wires, 4, WireColour::Red) == 0)
+  else if (wires[3] == WireColour::Yellow && countWiresByColour(wires, 4, WireColour::Red) == 0)
     return 0;
-  else if(countWiresByColour(wires, 4, WireColour::Blue) == 1)
+  else if (countWiresByColour(wires, 4, WireColour::Blue) == 1)
     return 0;
-  else if(countWiresByColour(wires, 4, WireColour::Yellow) > 1)
+  else if (countWiresByColour(wires, 4, WireColour::Yellow) > 1)
     return 3;
   else
     return 1;
@@ -177,11 +194,11 @@ int8_t findSolutionFourWires(WireColour* wires, bool serialOdd)
 
 int8_t findSolutionFiveWires(WireColour* wires, bool serialOdd)
 {
-  if(serialOdd && wires[4] == WireColour::Black)
+  if (serialOdd && wires[4] == WireColour::Black)
     return 3;
-  else if(countWiresByColour(wires, 5, WireColour::Red) == 1 && countWiresByColour(wires, 5, WireColour::Yellow) > 1)
+  else if (countWiresByColour(wires, 5, WireColour::Red) == 1 && countWiresByColour(wires, 5, WireColour::Yellow) > 1)
     return 0;
-  else if(countWiresByColour(wires, 5, WireColour::Black) == 0)
+  else if (countWiresByColour(wires, 5, WireColour::Black) == 0)
     return 1;
   else
     return 0;
@@ -189,11 +206,11 @@ int8_t findSolutionFiveWires(WireColour* wires, bool serialOdd)
 
 int8_t findSolutionSixWires(WireColour* wires, bool serialOdd)
 {
-  if(serialOdd && countWiresByColour(wires, 6, WireColour::Yellow) == 0)
+  if (serialOdd && countWiresByColour(wires, 6, WireColour::Yellow) == 0)
     return 2;
-  else if(countWiresByColour(wires, 6, WireColour::Yellow) == 1 && countWiresByColour(wires, 6, WireColour::White) > 1)
+  else if (countWiresByColour(wires, 6, WireColour::Yellow) == 1 && countWiresByColour(wires, 6, WireColour::White) > 1)
     return 3;
-  else if(countWiresByColour(wires, 6, WireColour::Red) == 0)
+  else if (countWiresByColour(wires, 6, WireColour::Red) == 0)
     return 5;
   else
     return 3;
@@ -202,9 +219,9 @@ int8_t findSolutionSixWires(WireColour* wires, bool serialOdd)
 int8_t countWiresByColour(WireColour* wires, int8_t numWires, WireColour colour)
 {
   int8_t result = 0;
-  for(int8_t i=0; i<numWires; ++i)
+  for (int8_t i = 0; i < numWires; ++i)
   {
-    if(wires[i] == colour)
+    if (wires[i] == colour)
       ++result;
   }
   return result;
@@ -213,9 +230,9 @@ int8_t countWiresByColour(WireColour* wires, int8_t numWires, WireColour colour)
 int8_t positionOfLastWireByColour(WireColour* wires, int8_t numWires, WireColour colour)
 {
   int8_t result = -1;
-  for(int8_t i=0; i<numWires; ++i)
+  for (int8_t i = 0; i < numWires; ++i)
   {
-    if(wires[i] == colour)
+    if (wires[i] == colour)
       result = i;
   }
   return result;
