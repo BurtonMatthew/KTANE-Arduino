b3bbd7f39a512bfcfe60e56e7fcd6713708432f4
Matt Burton <burtonmatthewa@gmail.com>
2022-11-22 23:13:20 -0500

    State-driven simon says

--- simonSays/simonSays.ino
+++ simonSays/simonSays.ino
@@ -1,5 +1,5 @@
 // Simon Says
-
+#include <BicolourLED.h>
 #include <Button.h>
 
 #define RemindDelay 5000
@@ -7,7 +7,11 @@
 #define FlashTime 500
 #define SequenceLength 4
 
+constexpr uint32_t failLedDisplayDuration = 300;
+
 //Pins
+#define LED_RED 3
+#define LED_GREEN 4
 #define ButtonRed 5
 #define ButtonBlue 6
 #define ButtonGreen 7
@@ -19,14 +23,14 @@
 
 enum class Colour
 {
-  Red,
-  Blue,
-  Green,
-  Yellow,
+  Red = 0,
+  Blue = 1,
+  Green = 2,
+  Yellow = 3,
   Count
 };
 
-// [SerialHasValue][Strikes][FlashingColour]
+// [SerialHasVowel][Strikes][FlashingColour]
 Colour SolutionMappings[2][3][(int)Colour::Count] =
 {
   // No Vowel in Serial
@@ -46,118 +50,290 @@ Colour SolutionMappings[2][3][(int)Colour::Count] =
 
 Button Buttons[(int)Colour::Count] =
 {
-  Button(ButtonRed, PULLUP),
-  Button(ButtonBlue, PULLUP),
-  Button(ButtonYellow, PULLUP), 
-  Button(ButtonGreen, PULLUP)
+  Button(ButtonRed, INPUT_PULLUP),
+  Button(ButtonBlue, INPUT_PULLUP),
+  Button(ButtonGreen, INPUT_PULLUP), 
+  Button(ButtonYellow, INPUT_PULLUP)
 };
 
-int Lights[(int)Colour::Count] = { LightRed, LightBlue, LightYellow, LightGreen };
+int Lights[(int)Colour::Count] = { LightRed, LightBlue, LightGreen, LightYellow };
+BicolourLED resultLed(LED_RED, LED_GREEN);
 
-unsigned long lastInputTime;
+
 Colour Sequence[SequenceLength];
-int SequenceStep;
-int UserSequenceStep;
+uint32_t ErrorFlashTime = 0;
+uint8_t Stage = 0;
+uint8_t PressedButton = 255;
+uint32_t LastButtonPressTime[4] = {0};
 
-int Strikes = 0;
-int Vowel = 0;
-
-bool DoingRemind;
-unsigned long RemindStartTime;
-
-void setup() 
+enum class State
 {
+  Setup,
+  Idle,
+  Remind,
+  Input,
+  Done
+};
+
+void ChangeState(State nextState);
+
+bool CheckButtonPressed()
+{
+  PressedButton = 255;
+  for(uint8_t i=0; i<(int)Colour::Count; ++i)
+  {
+    if(Buttons[i].uniquePress() && )
+    {
+      if(LastButtonPressTime[i] + 150 < millis())
+      {
+        PressedButton = i;
+      }
+      LastButtonPressTime[i] = millis();
+    }
+  }
+
+  return PressedButton < 255;
+}
+
+class StateSetup
+{
+  public:
+    static void Enter() 
+    {
+      Stage = 1;
+      for(int i=0; i<SequenceLength; ++i)
+      {
+        Sequence[i] = (Colour) random((int) Colour::Count);
+      }
+    }
+    static void Exit()  {}
+    static void Update() 
+    {
+      ChangeState(State::Idle);
+    }
+  private:
+};
+
+class StateIdle
+{
+  public:
+    static void Enter() 
+    {
+      enterTime = millis();
+    }
+    static void Exit() {}
+    static void Update()
+    {
+      uint32_t time = millis();
+      if(CheckButtonPressed())
+      {
+        ChangeState(State::Input);
+      }
+      else if(time > enterTime + RemindDelay)
+      {
+        ChangeState(State::Remind);
+      }
+    }
+  private:
+   static uint32_t enterTime;
+};
+uint32_t StateIdle::enterTime = 0;
+
+class StateRemind
+{
+  public:
+    static void Enter() 
+    {
+      enterTime = millis();
+      
+    }
+    static void Exit() 
+    {
+      for(uint8_t i = 0; i<(int)Colour::Count; ++i)
+      {
+        digitalWrite(Lights[i], LOW);
+      }
+    }
+    static void Update()
+    {
+      uint32_t seqTime = millis() - enterTime;
+      uint32_t step = seqTime / (FlashTime + RemindFlashGap);
+      uint32_t stepTime = seqTime % (FlashTime + RemindFlashGap);
+      
+      if(CheckButtonPressed())
+      {
+        ChangeState(State::Input);
+      }
+      else if(step >= Stage)
+      {
+        ChangeState(State::Idle);
+      }
+      else
+      {
+        digitalWrite(Lights[(int)Sequence[step]], stepTime < FlashTime);
+      }
+    }
+    private:
+      static uint32_t enterTime;
+};
+uint32_t StateRemind::enterTime = 0;
+
+class StateInput
+{
+  public:
+    static void Enter() 
+    { 
+      inputStage = 0;
+    }
+    static void Exit() 
+    {
+      for(uint8_t i = 0; i<(int)Colour::Count; ++i)
+      {
+        digitalWrite(Lights[i], LOW);
+      }
+    }
+    static void Update()
+    {
+      // Set LEDs
+      for(uint8_t i = 0; i < (int)Colour::Count; ++i)
+      {
+        digitalWrite(Lights[i], !digitalRead(ButtonRed + i));
+      }
+
+      if(PressedButton < 255)
+      {
+        if((uint8_t)SolutionMappings[0][0][(int)Sequence[inputStage]] == PressedButton)
+        {
+          ++inputStage;
+          if(inputStage >= Stage)
+          {
+            ++Stage;
+            if(Stage > SequenceLength)
+            {
+              ChangeState(State::Done);
+            }
+            else
+            {
+              ChangeState(State::Idle);
+            }
+          }
+        }
+        else
+        {
+            ErrorFlashTime = millis();
+            resultLed.write(BicolourLED::Colour::Red);
+            ChangeState(State::Idle);
+        }
+      }
+
+      CheckButtonPressed();
+    }
+  private:
+    static uint8_t inputStage;
+};
+uint8_t StateInput::inputStage = 0;
+
+class StateDone
+{
+  public:
+    static void Enter()
+    {
+      resultLed.write(BicolourLED::Colour::Green);
+    }
+    static void Exit() {}
+    static void Update() 
+    {
+      // Todo power saving sleep
+      delay(1000);
+    }
+};
+
+State CurrentState = State::Idle;
+
+void ChangeState(State nextState)
+{
+  switch(CurrentState)
+  {
+    case State::Setup:
+      StateSetup::Exit();
+      break;
+    case State::Idle:
+      StateIdle::Exit();
+      break;
+    case State::Remind:
+      StateRemind::Exit();
+      break;
+    case State::Input:
+      StateInput::Exit();
+      break;
+    case State::Done:
+      StateDone::Exit();
+      break;
+  }
+
+  switch(nextState)
+  {
+    case State::Setup:
+      StateSetup::Enter();
+      break;
+    case State::Idle:
+      StateIdle::Enter();
+      break;
+    case State::Remind:
+      StateRemind::Enter();
+      break;
+    case State::Input:
+      StateInput::Enter();
+      break;
+    case State::Done:
+      StateDone::Enter();
+      break;
+  }
+
+  CurrentState = nextState;
+}
+
+void setup()
+{
+  int seed = 0;
+  for(int i=0; i<100; ++i)
+  {
+    seed += analogRead(0);
+  }
+  randomSeed(seed);
+
   for(int i=0; i<(int)Colour::Count; ++i)
   {
     pinMode(Lights[i], OUTPUT);
   }
-  startModule();
+  resultLed.begin();
+
+  ChangeState(State::Setup);
 }
 
-void loop() 
+void loop()
 {
-  unsigned long Time = millis();
-  /*
-  if(Buttons[(int)Colour::Red].isPressed())
+  switch(CurrentState)
   {
-    lastInputTime = millis();
-    digitalWrite(LightRed, HIGH);
-  }
-  */
-
-  for(int i=0; i<(int)Colour::Count; ++i)
-  {
-    if(Buttons[i].uniquePress())
-    {
-      lastInputTime = Time;
-      DoingRemind = false;
-
-      // Correct answer?
-      if((int) SolutionMappings[Vowel][Strikes][(int)Sequence[UserSequenceStep]] == i)
-      {
-        UserSequenceStep++;
-
-        // Done with this pattern?
-        if(UserSequenceStep == SequenceStep)
-        {
-          UserSequenceStep = 0;
-          SequenceStep++;
-          lastInputTime = lastInputTime - RemindDelay + (RemindFlashGap * 3);
-        }
-      }
-      else
-      {
-        // we got it wrong womp womp
-        Strikes++;
-        UserSequenceStep = 0;
-        lastInputTime = lastInputTime - RemindDelay + (RemindFlashGap * 3);
-      }
-    }
+    case State::Setup:
+      StateSetup::Update();
+      break;
+    case State::Idle:
+      StateIdle::Update();
+      break;
+    case State::Remind:
+      StateRemind::Update();
+      break;
+    case State::Input:
+      StateInput::Update();
+      break;
+    case State::Done:
+      StateDone::Update();
+      break;
   }
 
-  // Do remind if its been too long since last input
-  if(!DoingRemind && lastInputTime + RemindDelay > Time)
+  if(CurrentState != State::Done && millis() > ErrorFlashTime + failLedDisplayDuration)
   {
-    DoingRemind = true;
-    RemindStartTime = Time;
-    digitalWrite(LightRed, LOW);
-  }
-
-  // Stop Remind if we've finished it
-  if(DoingRemind && Time > RemindStartTime + ( SequenceStep * (FlashTime + RemindFlashGap) ) )
-  {
-    DoingRemind = false;
-    lastInputTime = Time;
-  }
-
-  // Do Remind flashes
-  if(DoingRemind)
-  {
-    for(int i=0; i<SequenceStep; ++i)
-    {
-      if(Time > i * (FlashTime + RemindFlashGap) && Time < (i+1) * (FlashTime + RemindFlashGap) - RemindFlashGap)
-      {
-        digitalWrite(Lights[(int)Sequence[i]], HIGH);
-      }
-      else
-      {
-        digitalWrite(Lights[(int)Sequence[i]], LOW);
-      }
-    }
+    resultLed.write(BicolourLED::Colour::Off);
   }
 }
-
-void startModule()
-{
-  for(int i=0; i<SequenceLength; ++i)
-  {
-    Sequence[i] = (Colour) random((int) Colour::Count);
-  }
-
-  SequenceStep = 1;
-  UserSequenceStep = 0;
-  lastInputTime = 0;
-  DoingRemind = false;
-  RemindStartTime = 0;
-}
-
